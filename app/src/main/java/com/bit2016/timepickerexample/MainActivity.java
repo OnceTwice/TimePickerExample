b4fa836d3ea07c6f290400f5f5f834edbe2ae4c1
cho <you-smile@nate.com>
2016-11-28 06:18:28 +0900

    1

--- app/src/main/java/com/bit2016/timepickerexample/MainActivity.java
+++ app/src/main/java/com/bit2016/timepickerexample/MainActivity.java
@@ -2,12 +2,21 @@ package com.bit2016.timepickerexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DatePicker datePicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        datePicker = (DatePicker)findViewById( R.id.datePicker );
+
     }
 }
